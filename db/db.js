318d8de1d35b539aea1ddcec40e996ba3c41635f
harshitsharmamern <84194385+harshitsharmamern@users.noreply.github.com>
2021-11-02 18:25:07 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const db = 'mongodb+srv://29oct:29oct@cluster0.sfng4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 // const db = 'mongodb+srv://instaclone:instaclone@cluster0.kmfyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const db = "mongodb://localhost:27017/3"
+// const db = "mongodb://localhost:27017/3"
 mongoose.connect(db,{
 
 useNewUrlParser:true,
